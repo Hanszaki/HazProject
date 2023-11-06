606787d65d2fc2ba2628858c4cd0259ffb149e07
Hanszaki <149482393+Hanszaki@users.noreply.github.com>
2023-11-06 16:56:08 +0700

    main.js

--- to do list/main.js
+++ to do list/main.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const todoForm = document.getElementById("new-todo-form");
     const todoList = document.getElementById("todo-list");
 
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    for (const task of storedTasks) {
+        createTodoItem(task.content, task.category, task.completed);
+    }
+
     todoForm.addEventListener("submit", function (event) {
         event.preventDefault();
 
@@ -17,38 +22,13 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         if (taskInput.value.trim() !== "" && selectedCategory !== null) {
-            const todoItem = document.createElement("div");
-            todoItem.className = "todo-item";
-
-            const label = document.createElement("label");
-            label.innerHTML = `
-                <input type="checkbox">
-                <span class="bubble ${selectedCategory}"></span>
-            `;
-
-            const todoContent = document.createElement("div");
-            todoContent.className = "todo-content";
-            todoContent.innerHTML = `
-                <input type="text" value="${taskInput.value}" readonly>
-            `;
-
-            const actions = document.createElement("div");
-            actions.className = "actions";
-            actions.innerHTML = `
-                <button class="edit">Edit</button>
-                <button class="delete">Hapus</button>
-            `;
-
-            todoItem.appendChild(label);
-            todoItem.appendChild(todoContent);
-            todoItem.appendChild(actions);
-
-            todoList.appendChild(todoItem);
-
+            createTodoItem(taskInput.value, selectedCategory, false);
             taskInput.value = "";
             for (const categoryInput of categoryInputs) {
                 categoryInput.checked = false;
             }
+
+            saveTasksToLocalStorage();
         }
     });
 
@@ -59,11 +39,24 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (event.target.classList.contains("delete")) {
             const todoItem = event.target.parentElement.parentElement;
             todoList.removeChild(todoItem);
+
+            saveTasksToLocalStorage();
+        } else if (event.target.type === "checkbox") {
+            const todoContentInput = event.target.parentElement.parentElement.querySelector(".todo-content input");
+            if (event.target.checked) {
+                todoContentInput.style.textDecoration = "line-through";
+            } else {
+                todoContentInput.style.textDecoration = "none";
+            }
         }
     });
 
     const modeToggle = document.getElementById("mode-toggle");
     const modeLabel = document.getElementById("mode-label");
+
+    const isDarkMode = JSON.parse(localStorage.getItem("darkMode")) || false;
+
+    modeToggle.checked = isDarkMode;
 
     modeToggle.addEventListener("change", function () {
         if (modeToggle.checked) {
@@ -73,5 +66,56 @@ document.addEventListener("DOMContentLoaded", function () {
             document.body.classList.remove("dark-mode");
             modeLabel.innerText = "Light Mode";
         }
+
+        localStorage.setItem("darkMode", JSON.stringify(modeToggle.checked));
     });
+
+    if (isDarkMode) {
+        document.body.classList.add("dark-mode");
+        modeLabel.innerText = "Dark Mode";
+    }
+
+    function createTodoItem(content, category, completed) {
+        const todoItem = document.createElement("div");
+        todoItem.className = "todo-item";
+
+        const label = document.createElement("label");
+        label.innerHTML = `
+            <input type="checkbox" ${completed ? "checked" : ""}>
+            <span class="bubble ${category}"></span>
+        `;
+
+        const todoContent = document.createElement("div");
+        todoContent.className = "todo-content";
+        todoContent.innerHTML = `
+            <input type="text" value="${content}" ${completed ? 'readonly style="text-decoration: line-through;"' : ""}>
+        `;
+
+        const actions = document.createElement("div");
+        actions.className = "actions";
+        actions.innerHTML = `
+            <button class="edit">Edit</button>
+            <button class="delete">Hapus</button>
+        `;
+
+        todoItem.appendChild(label);
+        todoItem.appendChild(todoContent);
+        todoItem.appendChild(actions);
+
+        todoList.appendChild(todoItem);
+
+        saveTasksToLocalStorage();
+    }
+
+    function saveTasksToLocalStorage() {
+        const tasks = [];
+        const todoItems = document.querySelectorAll(".todo-item");
+        for (const todoItem of todoItems) {
+            const content = todoItem.querySelector(".todo-content input").value;
+            const category = todoItem.querySelector(".bubble").classList[1];
+            const completed = todoItem.querySelector("input[type='checkbox']").checked;
+            tasks.push({ content, category, completed });
+        }
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
 });
